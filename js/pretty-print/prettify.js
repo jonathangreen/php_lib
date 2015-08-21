cb61f634b097e3f0f6f2153b4651da431c6aab9b
nruest <ruestn@gmail.com>
2015-08-21 15:42:58 -0400

    Add TravisCI to php_lib, and codingstandards ignore blocks, and update maintainer in README. Address ISLANDORA-1080.

--- js/pretty-print/prettify.js
+++ js/pretty-print/prettify.js
@@ -1,3 +1,4 @@
+// @codingStandardsIgnoreStart
 // Copyright (C) 2006 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 
 /**
  * @fileoverview
@@ -1506,3 +1506,4 @@ window['_pr_isIE6'] = function () {
         'PR_TYPE': PR_TYPE
       };
 })();
+// @codingStandardsIgnoreEnd
